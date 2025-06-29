5b5624c43b9d53fc8a59fa671625980677ba8ecb
hk-durrani <durrani0305@gmail.com>
2025-06-29 15:06:27 +0500

    New Responsive menu added

--- js/script.js
+++ js/script.js
@@ -29,3 +29,10 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+});
